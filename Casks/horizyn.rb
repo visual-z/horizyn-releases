fae0546a48e4ae2d3a1b6a3fa084f1798c58e733
Visual <visual_zyh@foxmail.com>
2026-09-10 15:51:26 +0800

    Publish Horizyn 0.1.7

--- Casks/horizyn.rb
+++ Casks/horizyn.rb
@@ -1,6 +1,6 @@
 cask "horizyn" do
-  version "0.1.6"
-  sha256 "b53a3e3f6774649e6d378ea558cf6ff36581d24237c9e244e8d7213393dd77ad"
+  version "0.1.7"
+  sha256 "62ef104a27e85437e8e6e467af8afa3fdebdcb4c4c56df14f2efc7abd47e64e3"
 
   url "https://github.com/visual-z/horizyn-releases/releases/download/v#{version}/Horizyn-#{version}-arm64.dmg"
   name "Horizyn"
